cab6e91f5dbdd16c323e31b1bddb2554148f3de7
Xinyuan Xu <xinyuan.xu@gmail.com>
2015-02-22 20:37:14 +0800

    Init

---
+++ BrainBurst/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrainBurst
+//
+//  Created by xuxinyuan on 2/22/15.
+//  Copyright (c) 2015 Last4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
