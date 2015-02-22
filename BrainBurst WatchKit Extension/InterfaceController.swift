f7326144fa5c14554f2d0cb6e32e71817667ff6e
Xinyuan Xu <xinyuan.xu@gmail.com>
2015-02-22 20:46:50 +0800

    Added WatchKit target

---
+++ BrainBurst WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  BrainBurst WatchKit Extension
+//
+//  Created by xuxinyuan on 2/22/15.
+//  Copyright (c) 2015 Last4. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
